39cf1b27e9b892aed98c984aec526bb168b453e4
Jim Bracewell <jimbracewell@Jims-MacBook-Air.local>
2014-01-31 17:42:45 +0000

    many many fixes, now works as a feedworward net properly. still need to implement learning

--- main.m
+++ main.m
@@ -1,52 +1,56 @@
 load('spikeTime_inputs.mat');
-input_fire_times = peakLocs;
+input_fire_times = zeros(8,1);
 load('spikeTimings_desired.mat');
 desired_output_fire_times = peakLocs;
 
-inputNeuron;
+
 %20 connections -one from each input to each hidden
 %20 connections - one from each output to the output
 
 %need to keep for each neuron:
 %1. a list of times they have fired
 %2. a list of the weights coming towards them
-nodeCount = 8;
+node_count = 8;
 input_hidden_weights = randn(node_count,1);
 hidden1_fire_times = zeros(node_count,20);
 
 hidden1_hidden2_weights = randn(node_count,node_count);
-hidden2_fire_times = randn(20,20);
+hidden2_fire_times =  zeros(node_count,20);
 
-hidden2_output_weights = randn(20,1);
-output_fire_times = zeros(20,20);
-threshold = 1;
+hidden2_output_weights = randn(node_count,1);
+output_fire_times = zeros(node_count,20);
+threshold = 0.1;
 
 %run the network
-pointer = 0;
-for i = 1:0.0001:20
+pointer = 1;
+for i = 0:0.0001:20
     %check if input neuron has fired
-    if i >= input_fire_times(pointer)
-        pointer = pointer + 1;
+    if i >= peakLocs(1,pointer)
+        input_fire_times(pointer) = peakLocs(1,pointer);
+        if (pointer < node_count)
+            pointer = pointer + 1;
+        end
+        
     end
     
     %check if the hidden neurons have fired
-    for j = 1:nodeCount
-        potential = hiddenPotential(j, i, input_hidden_weights, input_fire_times(j,:));
+    for j = 1:node_count
+        potential = hiddenPotential(j, i, input_hidden_weights, input_fire_times, hidden1_fire_times(j,:), threshold);
         
         %if passes the threshold, add a firing time to that neuron
         if potential >= threshold
-            hidden1_fire_times(nnz(hidden1_fire_times)+1) = i;     
+            hidden1_fire_times(j,nnz(hidden1_fire_times)+1) = i;     
         end
         
     end
     
     %check if the 2nd layer hidden neurons have fired
-     for j = 1:nodeCount
-        potential = hiddenPotential(j, i, hidden1_hidden2_weights, hidden1_fire_times(j,:));
+     for j = 1:node_count
+        potential = hiddenPotential(j, i, hidden1_hidden2_weights, hidden1_fire_times(j,:), hidden2_fire_times(j,:), threshold);
         
         %if passes the threshold, add a firing time to that neuron
         if potential >= threshold
-            hidden2_fire_times(nnz(hidden2_fire_times)+1) = i;     
+            hidden2_fire_times(j,nnz(hidden2_fire_times(j,:))+1) = i;     
         end
         
     end
@@ -57,10 +61,10 @@ for i = 1:0.0001:20
     
     %IDEALLY HAVE 2 HIDDEN LAYERS, BUT MAKE WORK WITH 1 HIDDEN LAYER FOR
     %NOW
-    for j = 1:nodeCount
-        potential = hiddenPotential(j, i, hidden2_output_weights);
+    for j = 1:node_count
+        potential = hiddenPotential(j, i, hidden2_output_weights, hidden2_fire_times(j,:), output_fire_times(j,:), threshold);
         if potential >= threshold
-            output_fire_times(nnz(hidden_fire_times) + 1) = i;
+            output_fire_times(j,nnz(output_fire_times(j,:)) + 1) = i;
 
         end
     end
--- spikeResponse.m
+++ spikeResponse.m
@@ -1,18 +1,6 @@
 
-function rtn = spikeResponse(s)
-t_m = 0.05;
-t_s = 0.02;
 
-rtn = (exp(-s/t_m) - exp(-s/t_s)) * H(s);
 
-end
 
-function rtn = H(s)
-    if s <= 0
-        rtn = 0;
-    else
-        rtn = 1;
-    end
-       
-end
+
 
