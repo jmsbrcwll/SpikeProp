256427d9f20b913f57cea1f075740dc6164b80c7
Jim Bracewell <jimbracewell@dhcp-68-176.xlate.eduroam.ucl.ac.uk>
2014-02-01 15:12:18 +0000

    removed redundant comments and changed system so each non-input neuron fires only once

--- main.m
+++ main.m
@@ -8,17 +8,21 @@ desired_output_fire_times = peakLocs;
 %20 connections - one from each output to the output
 
 %need to keep for each neuron:
-%1. a list of times they have fired
+%1. a list of the fire time (if it has fired)
 %2. a list of the weights coming towards them
+
+%TO-DO: may need to alter so there are node_count input neurons
+%which fire once for each spike in the train, depending on how the error
+%backpropagation works at the input layer
 node_count = 8;
 input_hidden_weights = randn(node_count,1);
-hidden1_fire_times = zeros(node_count,20);
+hidden1_fire_times = zeros(node_count,1);
 
 hidden1_hidden2_weights = randn(node_count,node_count);
-hidden2_fire_times =  zeros(node_count,20);
+hidden2_fire_times =  zeros(node_count,1);
 
 hidden2_output_weights = randn(node_count,1);
-output_fire_times = zeros(node_count,20);
+output_fire_times = zeros(node_count,1);
 threshold = 0.1;
 
 %run the network
@@ -38,8 +42,8 @@ for i = 0:0.0001:20
         potential = hiddenPotential(j, i, input_hidden_weights, input_fire_times, hidden1_fire_times(j,:), threshold);
         
         %if passes the threshold, add a firing time to that neuron
-        if potential >= threshold
-            hidden1_fire_times(j,nnz(hidden1_fire_times)+1) = i;     
+        if potential >= threshold && hidden1_fire_times(j,1) == 0
+            hidden1_fire_times(j,1) = i;     
         end
         
     end
@@ -48,23 +52,18 @@ for i = 0:0.0001:20
      for j = 1:node_count
         potential = hiddenPotential(j, i, hidden1_hidden2_weights, hidden1_fire_times(j,:), hidden2_fire_times(j,:), threshold);
         
-        %if passes the threshold, add a firing time to that neuron
-        if potential >= threshold
-            hidden2_fire_times(j,nnz(hidden2_fire_times(j,:))+1) = i;     
+        %if passes the threshold, set the fire time
+        if potential >= threshold && hidden2_fire_times(j,1) == 0
+            hidden2_fire_times(j,1) = i;     
         end
         
     end
     
-    %check if the output neuron has fired
-    %NEED TO CHANGE SO THAT THERE ARE 8 OUTPUT NEURONS, AS SPIKEPROP ONLY
-    %ALLOWS FOR A SINGLE SPIKE
-    
-    %IDEALLY HAVE 2 HIDDEN LAYERS, BUT MAKE WORK WITH 1 HIDDEN LAYER FOR
-    %NOW
+ 
     for j = 1:node_count
         potential = hiddenPotential(j, i, hidden2_output_weights, hidden2_fire_times(j,:), output_fire_times(j,:), threshold);
-        if potential >= threshold
-            output_fire_times(j,nnz(output_fire_times(j,:)) + 1) = i;
+        if potential >= threshold && output_fire_times(j,1) == 0
+            output_fire_times(j,1) = i;
 
         end
     end
